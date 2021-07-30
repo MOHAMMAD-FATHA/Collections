6cf80335eb55fcfe0133bec7aa2b67cd716e366b
unknown <mfjkd7795@gmail.com>
2021-07-30 14:43:47 -0700

    Welcome to the Address Book Program using Collection

---
+++ src/com/bl/program/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bl.program;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Address Book Porgram");
+	}
+
+}
